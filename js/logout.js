ea5853ecba673c58b37f549e62ed0b166477b69c
Jpnoble1 <noblejohnpaul1o@gmail.com>
2024-07-24 07:25:41 +0800

    logout

--- js/logout.js
+++ js/logout.js
@@ -1,27 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get the Log Out link elements
   const logoutLinks = document.querySelectorAll(
     "#logout-link, #mobile-logout-link"
   );
 
-  // Add event listener for Log Out links
   logoutLinks.forEach((logoutLink) => {
     logoutLink.addEventListener("click", function (event) {
-      // Prevent default link behavior
       event.preventDefault();
 
-      // Confirm logout
       const confirmLogout = confirm("Are you sure you want to log out?");
 
       if (confirmLogout) {
-        // Clear session cookie
         document.cookie =
           "userEmail=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        // Optionally, redirect to login page or perform other logout actions
+
         alert("Logged out successfully!");
-        window.location.href = "./signin"; // Redirect to login page after logout
+        window.location.href = "./signin";
       } else {
-        // User canceled the logout
         alert("Logout canceled.");
       }
     });
